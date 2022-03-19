1408156cb0ecb8602ae3764bc26ad73164e72e2d
Liubov K <lyouso@yandex.ru>
2022-03-19 12:28:00 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+ThirdNumber(number);
+
+void ThirdNumber(string number1)
+{
+    if (number1.Length <= 2)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else {
+        Console.WriteLine(number1[2]);
+    }
+}
